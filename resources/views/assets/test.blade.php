7c9c788af4549ecaecd6bbd93db9b8305f055e3e
badhon229 <badhonsarkar518@gmail.com>
2020-11-29 00:51:38 +0600

    homegallery

--- resources/views/assets/test.blade.php
+++ resources/views/assets/test.blade.php
@@ -1,17 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>DAyCARe member signup</title>
 
-    <!-- Font Icon -->
-    <link rel="stylesheet" href="userasset/fonts/material-icon/css/material-design-iconic-font.min.css">
-
-    <!-- Main css -->
-    <link rel="stylesheet" href="userasset/css/style.css">
-</head>
 <body>
 
     <div class="main">
